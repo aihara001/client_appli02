be9da2435583f8b41f595ffd4d3d511d7589920d
takeshi aihara <vshine01@gmail.com>
2018-08-27 20:37:25 +0000

    表示修正

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -26,18 +26,18 @@ class ClientsController < ApplicationController
 
   def show
     @tasks = @client.tasks
-    @payment01 = Task.where(task_payment_month: '1').sum(:task_payment)
-    @payment02 = Task.where(task_payment_month: '2').sum(:task_payment)
-    @payment03 = Task.where(task_payment_month: '3').sum(:task_payment)
-    @payment04 = Task.where(task_payment_month: '4').sum(:task_payment)
-    @payment05 = Task.where(task_payment_month: '5').sum(:task_payment)
-    @payment06 = Task.where(task_payment_month: '6').sum(:task_payment)
-    @payment07 = Task.where(task_payment_month: '7').sum(:task_payment)
-    @payment08 = Task.where(task_payment_month: '8').sum(:task_payment)
-    @payment09 = Task.where(task_payment_month: '9').sum(:task_payment)
-    @payment10 = Task.where(task_payment_month: '10').sum(:task_payment)
-    @payment11 = Task.where(task_payment_month: '11').sum(:task_payment)
-    @payment12 = Task.where(task_payment_month: '12').sum(:task_payment)
+    @payment01 = Task.where(client_id: @client).where(task_payment_month: '1').sum(:task_payment)
+    @payment02 = Task.where(client_id: @client).where(task_payment_month: '2').sum(:task_payment)
+    @payment03 = Task.where(client_id: @client).where(task_payment_month: '3').sum(:task_payment)
+    @payment04 = Task.where(client_id: @client).where(task_payment_month: '4').sum(:task_payment)
+    @payment05 = Task.where(client_id: @client).where(task_payment_month: '5').sum(:task_payment)
+    @payment06 = Task.where(client_id: @client).where(task_payment_month: '6').sum(:task_payment)
+    @payment07 = Task.where(client_id: @client).where(task_payment_month: '7').sum(:task_payment)
+    @payment08 = Task.where(client_id: @client).where(task_payment_month: '8').sum(:task_payment)
+    @payment09 = Task.where(client_id: @client).where(task_payment_month: '9').sum(:task_payment)
+    @payment10 = Task.where(client_id: @client).where(task_payment_month: '10').sum(:task_payment)
+    @payment11 = Task.where(client_id: @client).where(task_payment_month: '11').sum(:task_payment)
+    @payment12 = Task.where(client_id: @client).where(task_payment_month: '12').sum(:task_payment)
   end
 
   def edit
