b2b6b168f51db0e9d7c408a38610197beb065867
takeshi aihara <vshine01@gmail.com>
2018-08-28 14:13:20 +0000

    エラー表示修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,6 @@ class Task < ApplicationRecord
     validates :task_payment_year, presence: true, numericality: { only_integer: true, greater_than: 2017, less_than: 2020 }
     validates :task_payment_month, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 13 }
     validates :task_payment, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 999999999 }
+    validates :task_content, length: { in: 0..255 }
     belongs_to :client
 end
